801c7c2d07b38b3a4603a4552aa2e254a2e38938
Victoria Olivieri <70556187+vickyrose456@users.noreply.github.com>
2022-03-31 00:54:54 -0400

    Heroku error

--- server/router.js
+++ server/router.js
@@ -10,23 +10,23 @@ const router = (app) => {
   // app.get('/login', controllers.Account.loginPage);
   // app.post('/login', controllers.Account.login);
 
-  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signUpPage);
-  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
-  // app.get('/signup', controllers.Account.signUpPage);
-  // app.post('/signup', controllers.Account.signup);
+  // app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signUpPage);
+  // app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
+  app.get('/signup', controllers.Account.signUpPage);
+  app.post('/signup', controllers.Account.signup);
 
   // make sure theyre logged in and cant logout if they arent logged in
-  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
-  // app.get('/logout', controllers.Account.logout);
+  // app.get('/logout', mid.requiresLogin, controllers.Account.logout);
+  app.get('/logout', controllers.Account.logout);
 
   // make sure theyre logged in. otherwise they cannot reach page
-  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
-  app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
-  // app.get('/maker', controllers.Domo.makerPage);
-  // app.post('/maker', controllers.Domo.makeDomo);
+  // app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  // app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
+  app.get('/maker', controllers.Domo.makerPage);
+  app.post('/maker', controllers.Domo.makeDomo);
 
-  // app.get('/', controllers.Account.loginPage);
-  app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.get('/', controllers.Account.loginPage);
+  // app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
 module.exports = router;
